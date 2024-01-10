93ad44e0423e2c335235c7e2c73a08aa5262999b
Matt11Moriarty <86008091+Matt11Moriarty@users.noreply.github.com>
2024-01-10 15:52:18 -0700

    added vote model and seed

--- app/models/Post.py
+++ app/models/Post.py
@@ -10,8 +10,11 @@ class Post(Base):
     title = Column(String(100), nullable=False)
     post_url = Column(String(100), nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'))
+    vote_count = column_property(
+        select((func.count(Vote.id))).where(Vote.post_id == id)
+        )
     created_at = Column(DateTime, default=datetime.now)
     updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-
     user = relationship('User')
-    comments = relationship('Comment', cascade='all, delete')
+    comments = relationship('Comment', cascade='all,delete')
+    votes = relationship('Vote', cascade='all,delete')
